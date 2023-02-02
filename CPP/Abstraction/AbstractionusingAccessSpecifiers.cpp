75daac296bded30b472567b4e0f64cb5059bdbdd
Ajeet hundal <118430042+Ajeethundal@users.noreply.github.com>
2023-02-02 12:20:24 +0530

    Create AbstractionusingAccessSpecifiers.cpp

---
+++ CPP/Abstraction/AbstractionusingAccessSpecifiers.cpp
@@ -0,0 +1,35 @@
+// C++ Program to Demonstrate the
+// working of Abstraction
+#include <iostream>
+using namespace std;
+ 
+class implementAbstraction {
+private:
+    int a, b;
+ 
+public:
+    // method to set values of
+    // private members
+    void set(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+ 
+    void display()
+    {
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+    }
+};
+ 
+int main()
+{
+    implementAbstraction obj;
+    obj.set(10, 20);
+    obj.display();
+    return 0;
+}
+//output
+// a = 10
+// b = 20
